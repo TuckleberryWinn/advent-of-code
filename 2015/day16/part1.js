605ffedacc7d77d5fe76cc6fae05fa9820cbaa72
TuckleberryWinn <63176475+TuckleberryWinn@users.noreply.github.com>
2024-09-10 01:58:59 -0600

    Day 16
    Ended up not needing the match counter at all. Used it in part 1 hoping that it would be a more modular option in case part 2 required something goofy. Could have been slightly cleaner by just early returning each of those and having a success case at the bottom of the for loop instead.

---
+++ 2015/day16/part1.js
@@ -0,0 +1,51 @@
+const fs = require("node:fs");
+const path = require("node:path");
+const inputPath = path.join(path.dirname(require.main.filename), "input.txt");
+let input = fs.readFileSync(inputPath).toString().trim().split("\r\n");
+
+const auntSue = {
+  children: 3,
+  cats: 7,
+  samoyeds: 2,
+  pomeranians: 3,
+  akitas: 0,
+  vizslas: 0,
+  goldfish: 5,
+  trees: 3,
+  cars: 2,
+  perfumes: 1,
+};
+
+let possibleAuntSueMatches = [];
+// clean input and populate the possible match chart
+for (let i = 0; i < input.length; i++) {
+  let sueTemplate = {};
+  let splitInput = input[i].split(" ");
+  sueTemplate.number = Number(splitInput[1].slice(0, -1));
+
+  let firstProp = splitInput[2].slice(0, -1);
+  let firstVal = Number(splitInput[3].slice(0, -1));
+  sueTemplate[firstProp] = firstVal;
+
+  let secondProp = splitInput[4].slice(0, -1);
+  let secondVal = Number(splitInput[5].slice(0, -1));
+  sueTemplate[secondProp] = secondVal;
+
+  let thirdProp = splitInput[6].slice(0, -1);
+  let thirdVal = Number(splitInput[7].slice(0, -1));
+  sueTemplate[thirdProp] = thirdVal;
+
+  possibleAuntSueMatches.push(sueTemplate);
+}
+
+for (let i = 0; i < possibleAuntSueMatches.length; i++) {
+  let matchCount = 0;
+  for (const prop in possibleAuntSueMatches[i]) {
+    if (possibleAuntSueMatches[i][prop] === auntSue[prop]) {
+      matchCount += 1;
+    }
+    if (matchCount === 3) {
+      console.log(`Match found: ${possibleAuntSueMatches[i].number}`);
+    }
+  }
+}
