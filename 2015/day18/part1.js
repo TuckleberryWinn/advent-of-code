5cbfdd265184597f8052846b8767bfb0efea600a
TuckleberryWinn <63176475+TuckleberryWinn@users.noreply.github.com>
2024-09-13 03:12:13 -0600

    Day 19
    Loops, 2D arrays, and simple rules.  Conway's Game of Life, but elves.

---
+++ 2015/day18/part1.js
@@ -0,0 +1,83 @@
+const fs = require("node:fs");
+const path = require("node:path");
+const inputPath = path.join(path.dirname(require.main.filename), "input.txt");
+let input = fs.readFileSync(inputPath).toString().trim().split("\r\n");
+
+const NUMBER_OF_GENERATIONS = 100;
+
+//create 2D array of lights
+let lightGrid = Array(100)
+  .fill()
+  .map((rows) => Array(100).fill(false));
+
+//Go through input and turn on lights
+for (let i = 0; i < 100; i++) {
+  for (let j = 0; j < 100; j++) {
+    if (input[i][j] === "#") {
+      lightGrid[i][j] = true;
+    }
+  }
+}
+
+//Perform generations of game
+
+for (let i = 0; i < NUMBER_OF_GENERATIONS; i++) {
+  let nextGrid = Array(100)
+    .fill()
+    .map((rows) => Array(100).fill(false));
+
+  for (let j = 0; j < lightGrid.length; j++) {
+    for (let k = 0; k < lightGrid.length; k++) {
+      nextGrid[j][k] = checkNeighbors(j, k);
+    }
+  }
+  lightGrid = nextGrid;
+}
+
+function checkNeighbors(x, y) {
+  let adjacentLights = [
+    [x, y - 1],
+    [x + 1, y - 1],
+    [x + 1, y],
+    [x + 1, y + 1],
+    [x, y + 1],
+    [x - 1, y + 1],
+    [x - 1, y],
+    [x - 1, y - 1],
+  ];
+
+  let neighborCount = 0;
+  for (let i = 0; i < adjacentLights.length; i++) {
+    if (
+      adjacentLights[i][0] < 0 ||
+      adjacentLights[i][0] > 99 ||
+      adjacentLights[i][1] < 0 ||
+      adjacentLights[i][1] > 99
+    ) {
+      continue;
+    }
+    if (lightGrid[adjacentLights[i][0]][adjacentLights[i][1]]) {
+      neighborCount += 1;
+    }
+  }
+  //set value based on neighbor count
+  if (neighborCount === 3) {
+    return true;
+  } else if (neighborCount === 2 && lightGrid[x][y]) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//count remaining lights on
+let endLightCount = 0;
+for (let i = 0; i < lightGrid.length; i++) {
+  for (let j = 0; j < lightGrid.length; j++) {
+    if (lightGrid[i][j]) {
+      endLightCount += 1;
+    }
+  }
+}
+
+console.log(endLightCount);
