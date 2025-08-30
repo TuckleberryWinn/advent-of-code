06bba68c9334bd5850e8d22239eb9f75ddd1998b
TuckleberryWinn <63176475+TuckleberryWinn@users.noreply.github.com>
2025-08-30 17:00:03 -0600

    2016 day 2

--- 2016/day02/part1.ts
+++ 2016/day02/part1.ts
@@ -1,3 +1,38 @@
 const input = new TextDecoder("utf-8").decode(await Deno.readFile("input.txt"));
 
-console.log(input);
+const lines = input.split("\r\n");
+
+const inputs: number[] = [];
+
+const keypad: number[][] = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+for (let i = 0; i < lines.length; i++) {
+  const instructions = lines[i].split("");
+  const currentPos = {
+    vert: 0,
+    hor: 0,
+  };
+  for (const i in instructions) {
+    console.log(currentPos);
+    switch (instructions[i]) {
+      case "U":
+        currentPos.hor = Math.min(1, (currentPos.hor += 1));
+        break;
+      case "R":
+        currentPos.vert = Math.min(1, (currentPos.vert += 1));
+        break;
+      case "D":
+        currentPos.hor = Math.max(-1, (currentPos.hor -= 1));
+        break;
+      case "L":
+        currentPos.vert = Math.max(-1, (currentPos.vert -= 1));
+        break;
+    }
+  }
+  inputs.push(keypad[currentPos.hor + 1][currentPos.vert + 1]);
+}
+console.log(inputs);
--- 2016/day02/part2.ts
+++ 2016/day02/part2.ts
@@ -1,3 +1,41 @@
 const input = new TextDecoder("utf-8").decode(await Deno.readFile("input.txt"));
 
-console.log(input);
+const lines = input.split("\r\n");
+
+const inputs: string[] = [];
+
+const keypad: string[][] = [
+  ["x", "x", "1", "x", "x"],
+  ["x", "2", "3", "4", "x"],
+  ["5", "6", "7", "8", "9"],
+  ["x", "A", "B", "C", "x"],
+  ["x", "x", "D", "x", "x"],
+];
+
+const currentPos = {
+  vert: 2,
+  hor: 0,
+};
+
+for (let i = 0; i < lines.length; i++) {
+  const instructions = lines[i].split("");
+  for (const i in instructions) {
+    switch (instructions[i]) {
+      case "U":
+        currentPos.vert += Math.abs(currentPos.vert - 1) + Math.abs(currentPos.hor) > 2 ? 0 : -1;
+        break;
+      case "D":
+        currentPos.vert += Math.abs(currentPos.vert + 1) + Math.abs(currentPos.hor) > 2 ? 0 : 1;
+        break;
+      case "L":
+        currentPos.hor += Math.abs(currentPos.vert) + Math.abs(currentPos.hor - 1) > 2 ? 0 : -1;
+        break;
+      case "R":
+        currentPos.hor += Math.abs(currentPos.vert) + Math.abs(currentPos.hor + 1) > 2 ? 0 : 1;
+        break;
+    }
+  }
+  inputs.push(keypad[currentPos.vert + 2][currentPos.hor + 2]);
+}
+
+console.log(inputs);
