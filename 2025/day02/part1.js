da1daa889de55d426b65de26e2ef3e8995c4b3a0
Tuckleberry Winn <63176475+TuckleberryWinn@users.noreply.github.com>
2025-12-01 23:33:38 -0700

    2025 day 2

--- 2025/day02/part1.js
+++ 2025/day02/part1.js
@@ -1,4 +1,19 @@
 const fs = require("fs");
 let input = fs.readFileSync("input.txt").toString();
 
-console.log(result);
+const ranges = input.split(',')
+
+let sum = 0
+for (const range of ranges) {
+    const ends = range.split('-')
+
+    for (let x = Number(ends[0]); x <= Number(ends[1]); x++) {
+        if (String(x).length % 2 == 0){
+            if (String(x).slice(0, (String(x).length / 2)) == (String(x).slice(-(String(x).length / 2)))) {
+                sum += x
+            }
+        }
+    }
+}
+
+console.log(sum);
--- 2025/day02/part2.js
+++ 2025/day02/part2.js
@@ -1,4 +1,29 @@
 const fs = require("fs");
 let input = fs.readFileSync("input.txt").toString();
 
-console.log(instructionIndex);
+const ranges = input.split(',')
+
+let sum = 0
+for (const range of ranges) {
+    const ends = range.split('-')
+
+    for (let x = Number(ends[0]); x <= Number(ends[1]); x++) {
+        const digits = Array.from(String(x), Number)
+        const testObject = {}
+        for (const digit of digits) {
+           if (testObject[digit]) {
+            testObject[digit] += 1
+           } else {
+            testObject[digit] = 1
+           }
+        }
+
+        if (!Object.values(testObject).includes(1)) {
+            if (/^(\d+)\1+$/.test(x)){
+                sum += x
+            }
+        }
+    }
+}
+
+console.log(sum);
